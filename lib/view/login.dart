16b293dec0c7d5edcf68c807c0baccb01dfda636
Nazar <63929571+Drakenoff@users.noreply.github.com>
2021-08-22 16:52:35 +0300

    .

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -65,8 +65,6 @@ class _LoginState extends State<Login> {
                           _password.clear();
                         }
                         if (_formKey.currentState!.validate()) {
-                          // If the form is valid, display a snackbar. In the real world,
-                          // you'd often call a server or save the information in a database.
                           ScaffoldMessenger.of(context).showSnackBar(
                             const SnackBar(content: Text('not current data')),
                           );
